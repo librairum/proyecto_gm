1df445f0ac5f658b58fb79b125dc7ce743d00b59
Luiscq03 <callacaquispe@gmail.com>
2023-03-28 17:00:42 -0500

    Formulario Categoria
    Se creo , diseño y creo metodos para el formulario Categoria

--- src/proyecto_gm/Categoria/Categoria.java
+++ src/proyecto_gm/Categoria/Categoria.java
@@ -1,13 +1,30 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package proyecto_gm.Categoria;
 
-/**
- *
- * @author USUARIO
- */
 public class Categoria {
-    
+
+    protected String id, descripcion;
+
+    // Constructor
+    public Categoria(String id, String descripcion) {
+        this.id = id;
+        this.descripcion = descripcion;
+    }
+
+    // Getters & Setters
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
 }
