14ea0b06aea9087487840a262bd050f766f93eee
Emerson361 <emersonapazaquispe9@gmail.com>
2025-04-25 16:04:06 -0500

    archivos funcionando periodos, cuentasbancarias, viaticos, contactos

--- src/proyecto_gm/Cuentas/frmCuentas.java
+++ src/proyecto_gm/Cuentas/frmCuentas.java
@@ -79,8 +79,6 @@ public class frmCuentas extends javax.swing.JInternalFrame {
         tblCuentas = new javax.swing.JTable();
         jLabel1 = new javax.swing.JLabel();
         txtId = new javax.swing.JTextField();
-        jLabel8 = new javax.swing.JLabel();
-        txtFecha = new javax.swing.JFormattedTextField();
         jLabel2 = new javax.swing.JLabel();
         cboTipoPropietario = new javax.swing.JComboBox<>();
         jLabel4 = new javax.swing.JLabel();
@@ -181,11 +179,6 @@ public class frmCuentas extends javax.swing.JInternalFrame {
 
         jLabel1.setText("ID:");
 
-        jLabel8.setText("Fecha");
-
-        txtFecha.setFormatterFactory(new javax.swing.text.DefaultFormatterFactory(new javax.swing.text.DateFormatter(new java.text.SimpleDateFormat("dd/MM/yyyy"))));
-        txtFecha.setName(""); // NOI18N
-
         jLabel2.setText("Tipo Propietario:");
 
         cboTipoPropietario.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Persona Natural", "Empresa" }));
@@ -270,10 +263,6 @@ public class frmCuentas extends javax.swing.JInternalFrame {
                                 .addComponent(jLabel1)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                                 .addComponent(txtId, javax.swing.GroupLayout.PREFERRED_SIZE, 72, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                                .addComponent(jLabel8)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(txtFecha, javax.swing.GroupLayout.PREFERRED_SIZE, 99, javax.swing.GroupLayout.PREFERRED_SIZE)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                                 .addComponent(jLabel2)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
@@ -310,8 +299,6 @@ public class frmCuentas extends javax.swing.JInternalFrame {
                     .addComponent(btnCancelar))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addGroup(panelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(txtFecha, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabel8)
                     .addComponent(txtId, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel2)
                     .addComponent(cboTipoPropietario, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
@@ -469,7 +456,6 @@ public class frmCuentas extends javax.swing.JInternalFrame {
     private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel6;
     private javax.swing.JLabel jLabel7;
-    private javax.swing.JLabel jLabel8;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.ButtonGroup monedas;
     private javax.swing.JPanel panel;
@@ -478,7 +464,6 @@ public class frmCuentas extends javax.swing.JInternalFrame {
     private javax.swing.JTable tblCuentas;
     private javax.swing.JTextField txtCCC;
     private javax.swing.JTextField txtCCI;
-    private javax.swing.JFormattedTextField txtFecha;
     private javax.swing.JTextField txtId;
     private javax.swing.JTextField txtNombres;
     // End of variables declaration//GEN-END:variables
